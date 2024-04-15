484586cc9afc8ccf4dccb0f333e215dbb081e2bd
Shubham228197 <shubham228pandey@gmail.com>
2024-04-15 20:24:06 +0530

    Exercise Logic Done CSS remains

--- Manipulate_DOM/scriptEx3.js
+++ Manipulate_DOM/scriptEx3.js
@@ -5,6 +5,7 @@ import {
   ayTitles,
   aySkills,
   ayQualifications,
+  skillData,
 } from "../additionaldata.js";
 
 setInterval(() => {
@@ -44,8 +45,70 @@ const insertKeyword = () => {
   const keyHeading = document.createElement("h1");
   const keyBody = document.createElement("div");
   keyHeading.textContent = "Keywords";
-  keyBody.textContent = "Hi! i am body";
   keyDiv[0].appendChild(keyHeading);
-  keyDiv[0].appendChild(keyBody);
+  for (let i = 0; i < ayKeywords.length; i++) {
+    const keyWord = document.createElement("button");
+    keyWord.style.margin = "0px 3px 3px 3px";
+    keyWord.textContent = ayKeywords[i];
+    keyDiv[0].appendChild(keyWord);
+  }
 };
 insertKeyword();
+
+const insertAbout = () => {
+  for (let i = 0; i < ayTitles.length; i++) {
+    const titlesDiv = document.getElementsByClassName("titles");
+    const titlesContent = document.createElement("p");
+    titlesContent.textContent = ayTitles[i];
+    titlesDiv[0].appendChild(titlesContent);
+  }
+  for (let i = 0; i < aySkills.length; i++) {
+    const skillDiv = document.getElementsByClassName("skills");
+    const skillsContent = document.createElement("p");
+    skillsContent.textContent = aySkills[i];
+    skillDiv[0].appendChild(skillsContent);
+  }
+  for (let i = 0; i < ayQualifications.length; i++) {
+    const degreeDiv = document.getElementsByClassName("degree");
+    const degreeContent = document.createElement("p");
+    degreeContent.textContent = ayQualifications[i];
+    degreeDiv[0].appendChild(degreeContent);
+  }
+};
+insertAbout();
+
+const insertEvents = () => {
+  const containerDiv = document.querySelector(".container");
+  for (let i = 0; i < skillData.length; i++) {
+    const eventDiv = document.createElement("div");
+    eventDiv.setAttribute("class", "event");
+    const summPara = document.createElement("p");
+    const eventType = document.createElement("p");
+    eventType.style.textDecoration = "underline";
+    const eventDetails = document.createElement("details");
+    const eventSummary = document.createElement("summary");
+    const eventPara = document.createElement("p");
+    containerDiv.appendChild(eventDiv);
+    eventDiv.appendChild(eventType);
+    eventDiv.appendChild(eventDetails);
+    eventDetails.appendChild(eventSummary);
+    eventDetails.appendChild(summPara);
+    eventDiv.appendChild(eventPara);
+    eventType.textContent = skillData[i].course;
+    eventPara.textContent = skillData[i].type;
+    eventSummary.textContent = skillData[i].lang;
+    for (let item of skillData[i].summary) {
+      const techList = document.createElement("p");
+      techList.textContent = item;
+      summPara.appendChild(techList);
+    }
+    if (skillData[i].type == "Done") {
+      eventDiv.style.backgroundColor = "#4CCD99";
+    } else if (skillData[i].type == "Ongoing") {
+      eventDiv.style.backgroundColor = "#FFF78A";
+    } else {
+      eventDiv.style.backgroundColor = "#EF4040";
+    }
+  }
+};
+insertEvents();
