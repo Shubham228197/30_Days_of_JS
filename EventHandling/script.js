d250c72ab4d596b9e2f6f9c756d288e0b3f04a7f
Shubham228197 <shubham228pandey@gmail.com>
2024-04-21 14:32:42 +0530

    code optimization

--- EventHandling/script.js
+++ EventHandling/script.js
@@ -1,31 +1,40 @@
 const genBtn = document.querySelector(".btn");
 const userInp = document.querySelector("#userinp");
 
-genBtn.addEventListener("click", (e) => {
-  let genNumber = parseInt(userInp.value);
-  let primeNumArr = [];
-  const numType = [];
-  let c = 1;
-  for (let i = 0, p = 2; i <= genNumber; i++, p++) {
-    let flag = true;
-    for (let j = 2; j <= Math.sqrt(p); j++) {
-      if (p % j == 0 || p > genNumber) {
-        flag = false;
-        break;
+genBtn.addEventListener("click", () => {
+  const dispErr = document.getElementById("errorMsg");
+  let inpValue = parseInt(document.getElementById("userinp").value);
+  document.getElementById("container").innerHTML = "";
+  if (inpValue < 0 || isNaN(inpValue)) {
+    dispErr.textContent = "*input value is incorrect!";
+    dispErr.style.color = "red";
+  } else {
+    let genNumber = parseInt(userInp.value);
+    let primeNumArr = [];
+    const numType = [];
+    let c = 1;
+    dispErr.innerHTML = "\u00a0";
+    for (let i = 0, p = 2; i <= genNumber; i++, p++) {
+      let flag = true;
+      for (let j = 2; j <= Math.sqrt(p); j++) {
+        if (p % j == 0 || p > genNumber) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+        primeNumArr.push(p);
+      }
+      if (primeNumArr.includes(i)) {
+        numType.push({ num: i, type: "prime", color: "#EE4266" });
+      } else if (i % 2 == 0) {
+        numType.push({ num: i, type: "even", color: "#41B06E" });
+      } else {
+        numType.push({ num: i, type: "odd", color: "#F5DD61" });
       }
     }
-    if (flag) {
-      primeNumArr.push(p);
-    }
-    if (primeNumArr.includes(i)) {
-      numType.push({ num: i, type: "prime", color: "#EE4266" });
-    } else if (i % 2 == 0) {
-      numType.push({ num: i, type: "even", color: "#41B06E" });
-    } else {
-      numType.push({ num: i, type: "odd", color: "#F5DD61" });
-    }
+    numBlocks(genNumber, numType);
   }
-  numBlocks(genNumber, numType);
 });
 function numBlocks(genNumber, numType) {
   for (let i = 0; i <= genNumber; i++) {
@@ -33,15 +42,14 @@ function numBlocks(genNumber, numType) {
     const containerDiv = document.getElementsByClassName("container");
     element.style.backgroundColor = numType[i].color;
     element.textContent = numType[i].num;
-    element.style.padding = "2px";
-    element.style.margin = "2px";
-    element.style.color = "white";
-    element.style.fontSize = "50px";
-    element.style.width = "140px";
-    element.style.height = "55px";
-    element.style.display = "flex";
-    element.style.justifyContent = "center";
-    element.style.alignItems = "center";
+    element.setAttribute("class", "numBox");
     containerDiv[0].appendChild(element);
   }
 }
+
+// Optimization idea
+// add acknowledgement red text when user gived wrong input
+// number that are already geneated are pilling, fix it
+// improve JS code with adding class for hard coded CSS
+// resizing of website cause layout to go wrong: Fix it
+// declare box colors prior of the event
