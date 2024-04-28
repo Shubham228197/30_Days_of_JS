cdf095786c04283bb6966c815dec09274456774a
Shubham228197 <shubham228pandey@gmail.com>
2024-04-28 15:55:12 +0530

    Day28 Leaderboard completed

--- Leaderboard/script.js
+++ Leaderboard/script.js
@@ -1,35 +1,9 @@
-const scoreForm = document.getElementById("score-form");
-let rendercount = 0;
-let addedPlayerCount = 0;
-scoreForm.addEventListener("submit", (e) => {
-  // to save the URL from updating with data. CANCEL OUTS THE NORMAL BEHAVIOUR
-  e.preventDefault();
+import { leaderboardAccounts } from "../additionaldata.js";
+document.addEventListener("DOMContentLoaded", () => {
+  createComponent();
 });
-let JSONdata = [
-  {
-    id: "syd100",
-    fname: "SHUBHAM",
-    lname: "PANDEY",
-    country: "INDIA",
-    score: 100,
-  },
-  {
-    id: "ria50",
-    fname: "RAJU",
-    lname: "RAST0GI",
-    country: "MALAYSIA",
-    score: 50,
-  },
-  { id: "chs75", fname: "CHARLIE", lname: "PUTH", country: "USA", score: 75 },
-  {
-    id: "coi90",
-    fname: "CHANG LE",
-    lname: "SHAO",
-    country: "CHINA",
-    score: 90,
-  },
-];
-const onSubmit = async () => {
+
+document.getElementById("addingNewAccounts").addEventListener("click", () => {
   const firstName = document.getElementById("first-name");
   const lastName = document.getElementById("last-name");
   const countryName = document.getElementById("country");
@@ -37,53 +11,47 @@ const onSubmit = async () => {
   const id =
     firstName.value[0] +
     lastName.value[lastName.value.length - 1] +
-    countryName.value[countryName.value.length / 2] +
+    countryName.value[Math.floor(countryName.value.length / 2)] +
     scoreData.value;
-  return {
+  console.log("input id:", id);
+  addplayer({
     id: id.toLowerCase(),
     fname: firstName.value.toUpperCase(),
     lname: lastName.value.toUpperCase(),
     country: countryName.value.toUpperCase(),
     score: Number(scoreData.value),
-    // score: parseInt(scoreData.value, 10),
-  };
-};
-
-async function addplayer() {
-  rendercount++;
-  console.log("Render Count: ", rendercount);
-  sorting(JSONdata);
-  const userData = await onSubmit();
-  if (rendercount == 1) {
-    createComponent(JSONdata);
-  } else if (
-    userData.fname !== "" &&
-    userData.lname !== "" &&
-    userData.country !== "" &&
-    userData.score > 0 &&
-    rendercount > 1
+  });
+});
+function addplayer(newUser) {
+  console.log(newUser);
+  if (
+    newUser === undefined ||
+    newUser.id === NaN ||
+    newUser.fname == "" ||
+    newUser.lname == "" ||
+    newUser.country == "" ||
+    newUser.score == 0
   ) {
-    alert("I am Running");
-    JSONdata.push(userData);
-    sorting(JSONdata);
-    removeAllChildren();
-    createComponent(JSONdata);
+    alert("Invalid Entry");
   } else {
-    alert("Enter Valid data");
+    leaderboardAccounts.push(newUser);
+    removeAllChildren();
+    createComponent();
   }
 }
-
-function createComponent(JSONdata) {
-  // console.log(JSONdata);
-  for (let i = 0; i < JSONdata.length; i++) {
+async function createComponent() {
+  sortLeaderboards();
+  console.log("inside CreateComponent:", leaderboardAccounts);
+  for (let i = 0; i < leaderboardAccounts.length; i++) {
     const gridContainer = document.createElement("div");
     gridContainer.classList.add("grid-container");
-    gridContainer.setAttribute("id", JSONdata[i].id);
+    gridContainer.setAttribute("id", leaderboardAccounts[i].id);
     const gridItem_1 = document.createElement("div");
     gridItem_1.classList.add("grid-item");
     const username = document.createElement("p");
     username.setAttribute("id", "username");
-    username.innerText = JSONdata[i].fname + " " + JSONdata[i].lname;
+    username.innerText =
+      leaderboardAccounts[i].fname + " " + leaderboardAccounts[i].lname;
     const dateANDtime = document.createElement("p");
     dateANDtime.setAttribute("id", "current-data-time");
     dateANDtime.classList.add("timimg");
@@ -95,18 +63,24 @@ function createComponent(JSONdata) {
     const gridItem_2 = document.createElement("div");
     gridItem_2.setAttribute("id", "country-name");
     gridItem_2.classList.add("grid-item");
-    gridItem_2.innerText = JSONdata[i].country;
+    gridItem_2.innerText = leaderboardAccounts[i].country;
     gridContainer.appendChild(gridItem_2);
 
     const gridItem_3 = document.createElement("div");
-    gridItem_3.innerText = JSONdata[i].score;
+    gridItem_3.innerText = leaderboardAccounts[i].score;
     gridItem_3.setAttribute("id", "user-score");
     gridItem_3.classList.add("grid-item");
     gridContainer.appendChild(gridItem_3);
 
     const gridItem_4 = document.createElement("div");
     gridItem_4.classList.add("grid-item");
+
     const delButton = document.createElement("button");
+    delButton.addEventListener("click", async () => {
+      removeAccFromLeaderboard(i);
+      removeAllChildren();
+      createComponent();
+    });
     const binIcon = document.createElement("img");
     binIcon.src = "./images/delete.png";
     binIcon.alt = "del";
@@ -114,16 +88,29 @@ function createComponent(JSONdata) {
     delButton.classList.add("buttons");
     delButton.classList.add("cssbuttons");
     delButton.classList.add("delete-button");
+
     const pos5 = document.createElement("button");
     pos5.classList.add("cssbuttons");
     pos5.innerText = "+5";
     pos5.classList.add("buttons");
     pos5.classList.add("addScore-button");
+    pos5.addEventListener("click", async () => {
+      addFivePoints(i);
+      removeAllChildren();
+      createComponent();
+    });
+
     const neg5 = document.createElement("button");
     neg5.classList.add("cssbuttons");
     neg5.innerText = "-5";
     neg5.classList.add("buttons");
     neg5.classList.add("minusScore-button");
+    neg5.addEventListener("click", async () => {
+      minusFivePoints(i);
+      removeAllChildren();
+      createComponent();
+    });
+
     gridContainer.appendChild(gridItem_4);
     gridItem_4.appendChild(delButton);
     gridItem_4.appendChild(pos5);
@@ -133,122 +120,25 @@ function createComponent(JSONdata) {
     parentDiv.appendChild(gridContainer);
   }
 }
-
-// the way to delete all children of a div is to use removeChild and pass the firstChild inside a while loop as if there is a first child loop will continue and div child will keep deleting
 function removeAllChildren() {
   var myDiv = document.getElementById("score-cards");
   while (myDiv.firstChild) {
     myDiv.removeChild(myDiv.firstChild);
   }
 }
-function sorting(array) {
-  return array.sort((a, b) => b.score - a.score);
+
+function removeAccFromLeaderboard(index) {
+  leaderboardAccounts.splice(index, 1);
+  console.log("With Removed value:", leaderboardAccounts);
 }
-addplayer();
-// removing event handler is crusial as too many call were happening to activateButtons()
-function activateButtons() {
-  console.log("Active Buttons is called");
-  const deleteButtons = document.querySelectorAll(".delete-button");
-  function removeButtonEventListener(button, eventType, handler) {
-    button.removeEventListener(eventType, handler);
-  }
-  // Attach click event listener to each "delete-button"
-  deleteButtons.forEach((button) => {
-    console.log("inside FOR EACH is CAlled", button);
-    async function deleteDivContainer() {
-      console.log("inside deleteDivContainer()");
-      const container = button.parentNode;
-      const cont = container.parentNode;
-      const CntrID = cont.getAttribute("id");
-      cont.remove();
-      JSONdata = await removeJSONdata(CntrID, JSONdata);
-      removeButtonEventListener(button, "click", deleteDivContainer);
-    }
-    button.addEventListener("click", deleteDivContainer); // Use the once option here
+function sortLeaderboards() {
+  leaderboardAccounts.sort((a, b) => {
+    return b.score - a.score;
   });
 }
-
-// function activateButtons() {
-//   const deleteButtons = document.querySelectorAll(".delete-button");
-//   const addScore = document.querySelectorAll(".addScore-button");
-//   const minusScore = document.querySelectorAll(".minusScore-button");
-
-//   // Helper function to remove event listener from the button
-//   function removeButtonEventListener(button, eventType, handler) {
-//     button.removeEventListener(eventType, handler);
-//   }
-
-//   // Attach click event listener to each "addScore-button"
-//   addScore.forEach((button) => {
-//     async function addButtonClickHandler() {
-//       const container = button.parentNode;
-//       const cont = container.parentNode;
-//       const CntrID = cont.getAttribute("id");
-//       console.log("ADDED +5");
-//       JSONdata = await addScoreToID(CntrID);
-//       console.log("After: ", JSONdata);
-
-//       // Remove the event listener after handling the click once
-//       removeButtonEventListener(button, "click", addButtonClickHandler);
-//     }
-
-//     button.addEventListener("click", addButtonClickHandler);
-//   });
-
-//   minusScore.forEach((button) => {
-//     async function minusButtonClickHandler() {
-//       const container = button.parentNode;
-//       const cont = container.parentNode;
-//       const CntrID = cont.getAttribute("id");
-//       console.log("minus -5");
-//       JSONdata = await minusScoreToID(CntrID);
-//       console.log("After: ", JSONdata);
-
-//       // Remove the event listener after handling the click once
-//       removeButtonEventListener(button, "click", minusButtonClickHandler);
-//     }
-
-//     button.addEventListener("click", minusButtonClickHandler);
-//   });
-
-//   // Attach click event listener to each "delete-button"
-//   deleteButtons.forEach((button) => {
-//     async function deleteButtonClickHandler() {
-//       const container = button.parentNode;
-//       const cont = container.parentNode;
-//       const CntrID = cont.getAttribute("id");
-//       cont.remove();
-//       JSONdata = await removeJSONdata(CntrID, JSONdata);
-
-//       // Remove the event listener after handling the click once
-//       removeButtonEventListener(button, "click", deleteButtonClickHandler);
-//     }
-
-//     button.addEventListener("click", deleteButtonClickHandler);
-//   });
-// }
-
-// Get all buttons with class "delete-button"
-async function removeJSONdata(CntrID, JSONdata) {
-  return JSONdata.filter((item) => {
-    return item.id != CntrID;
-  });
+function addFivePoints(index) {
+  leaderboardAccounts[index].score += 5;
 }
-// async function addScoreToID(CntrID) {
-//   return JSONdata.map((item) => {
-//     if (CntrID === item.id) {
-//       return { ...item, score: item.score + 5 };
-//     } else {
-//       return item;
-//     }
-//   });
-// }
-// async function minusScoreToID(CntrID) {
-//   return JSONdata.map((item) => {
-//     if (CntrID === item.id) {
-//       return { ...item, score: item.score - 5 };
-//     } else {
-//       return item;
-//     }
-//   });
-// }
+function minusFivePoints(index) {
+  leaderboardAccounts[index].score -= 5;
+}
